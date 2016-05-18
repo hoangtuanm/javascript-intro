befe15694e565ba4a62c1d65b3188de143e0426b
Tuan Hoang <hoang.tuan@outlook.com>
2016-05-18 16:31:40 -0700

    Create 'variables/variables.js'

---
+++ variables/variables.js
@@ -0,0 +1,20 @@
+// Variable are just simply containers that store values
+// They follow this patter:
+
+var yourVariableName = yourValue;
+
+// They can store all of the values we've seen
+var name = "Rusty";
+var secretNumber = 73;
+var isAdorable = true;
+
+// Recall the stored value by calling the variable name
+var name = "Rusty";
+"hello there " + name	// "hello there Rusty"
+
+var num = 37;
+num + 3 + 10	// 50
+
+// Update existing variables
+var name = "Robert";
+name = "Bob";
