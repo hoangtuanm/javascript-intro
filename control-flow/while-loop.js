a05f95389a0170df93ad1da8394e5b75245202f1
Tuan Hoang <hoang.tuan@outlook.com>
2016-05-23 17:48:28 -0700

    Update control-flow/while-loops minus Excercise 3

--- control-flow/while-loop.js
+++ control-flow/while-loop.js
@@ -37,3 +37,30 @@ while (count < str.length) {
 	count++;
 } 
 
+//While Loops: Exercise 1
+var num = 1;
+
+while (num <= 10) {
+	console.log(num);
+	num += 2;
+}
+
+//While Loops: Exercise 2
+var num = 1
+
+while (num <= 20) {
+	if(num % 4 === 0) {
+		console.log(num);
+	}
+	num++;
+}
+
+
+
+
+
+
+
+
+
+
